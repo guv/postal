53b1b79e948d4211357b6599457e6d05fb9e45b1
Gunnar Völkel <Gunnar.Voelkel@uni-ulm.de>
2020-04-22 16:11:45 +0200

    Fixes pom version lookup.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.guv/postal "2.0.4"
+(defproject org.clojars.guv/postal "2.0.4.1"
   :description "JavaMail on Clojure"
   :url "https://github.com/drewr/postal"
   :license {:name "MIT"
